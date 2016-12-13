6ae0e8829fa40e887a63cdcca8f19419e4d2ebbb
David A. Ventimiglia <dventimi@gmail.com>
2016-12-13 15:30:42 -0800

    Added code from the lenet.py file

--- signs.py
+++ signs.py
@@ -74,6 +74,12 @@ for t in zip(range(n), np.random.choice(np.array(range(n_train)), n, False)):
 fig = plt.figure()
 h = plt.hist(train['labels'], n_classes)
 
+# Imports
+
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.contrib.layers import flatten
+
 ### Preprocess the data here.
 ### Feel free to use as many code cells as needed.
 
@@ -84,14 +90,106 @@ h = plt.hist(train['labels'], n_classes)
 ### Define your architecture here.
 ### Feel free to use as many code cells as needed.
 
+# Parameters
+
+EPOCHS = 10
+BATCH_SIZE = 50
+
+# LeNet architecture:
+# INPUT -> CONV -> ACT -> POOL -> CONV -> ACT -> POOL -> FLATTEN -> FC -> ACT -> FC
+#
+# Don't worry about anything else in the file too much, all you have to do is
+# create the LeNet and return the result of the last fully connected layer.
+def LeNet(x):
+    x = tf.reshape(x, (-1, 28, 28, 1))                                                       # 2D->4D for convolutional and pooling layers
+    x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], mode="CONSTANT")                         # Pad 0s->32x32, 2 rows/cols each side
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 1, 6)))                           # 28x28x6
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+    conv1 = tf.nn.relu(conv1)
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID') # 14x14x6
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16)))                          # 10x10x16
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    conv2 = tf.nn.relu(conv2)
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID') # 5x5x16
+    fc1 = flatten(conv2)                                                                     # Flatten
+    fc1_shape = (fc1.get_shape().as_list()[-1], 120)                                         # (5 * 5 * 16, 120)
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(fc1_shape)))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1 = tf.matmul(fc1, fc1_W) + fc1_b
+    fc1 = tf.nn.relu(fc1)
+    fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 10)))
+    fc2_b = tf.Variable(tf.zeros(10))
+    return tf.matmul(fc1, fc2_W) + fc2_b
+
+# MNIST consists of 28x28x1, grayscale images
+x = tf.placeholder(tf.float32, (None, 784))
+# Classify over 10 digits 0-9
+y = tf.placeholder(tf.float32, (None, 10))
+fc2 = LeNet(x)
+
+loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(fc2, y))
+opt = tf.train.AdamOptimizer()
+train_op = opt.minimize(loss_op)
+correct_prediction = tf.equal(tf.argmax(fc2, 1), tf.argmax(y, 1))
+accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+def eval_data(dataset):
+    """
+    Given a dataset as input returns the loss and accuracy.
+    """
+    # If dataset.num_examples is not divisible by BATCH_SIZE
+    # the remainder will be discarded.
+    # Ex: If BATCH_SIZE is 64 and training set has 55000 examples
+    # steps_per_epoch = 55000 // 64 = 859
+    # num_examples = 859 * 64 = 54976
+    #
+    # So in that case we go over 54976 examples instead of 55000.
+    steps_per_epoch = dataset.num_examples // BATCH_SIZE
+    num_examples = steps_per_epoch * BATCH_SIZE
+    total_acc, total_loss = 0, 0
+    sess = tf.get_default_session()
+    for step in range(steps_per_epoch):
+        batch_x, batch_y = dataset.next_batch(BATCH_SIZE)
+        loss, acc = sess.run([loss_op, accuracy_op], feed_dict={x: batch_x, y: batch_y})
+        total_acc += (acc * batch_x.shape[0])
+        total_loss += (loss * batch_x.shape[0])
+    return total_loss/num_examples, total_acc/num_examples
+
+
 ### Train your model here.
 ### Feel free to use as many code cells as needed.
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    steps_per_epoch = mnist.train.num_examples // BATCH_SIZE
+    num_examples = steps_per_epoch * BATCH_SIZE
+
+    # Train model
+    for i in range(EPOCHS):
+        for step in range(steps_per_epoch):
+            batch_x, batch_y = mnist.train.next_batch(BATCH_SIZE)
+            loss = sess.run(train_op, feed_dict={x: batch_x, y: batch_y})
+
+        val_loss, val_acc = eval_data(mnist.validation)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation loss = {:.3f}".format(val_loss))
+        print("Validation accuracy = {:.3f}".format(val_acc))
+        print()
+
 
 ### Load the images and plot them here.
 ### Feel free to use as many code cells as needed.
 
 ### Run the predictions here.
 ### Feel free to use as many code cells as needed.
+
+# Evaluate on the test data
+test_loss, test_acc = eval_data(mnist.test)
+print("Test loss = {:.3f}".format(test_loss))
+print("Test accuracy = {:.3f}".format(test_acc))
+
 
 ### Visualize the softmax probabilities here.
 ### Feel free to use as many code cells as needed.

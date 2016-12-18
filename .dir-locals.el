441d756a73557e6851c72c6ae2e6aa7943f8afe5
David A. Ventimiglia <dventimi@gmail.com>
2016-12-18 12:24:42 -0800

    Adding dir local vars file

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode
+  (org-html-postamble))
+ (python-mode
+  (comment-fill-column . 192)
+  (conda-project-env-name . "CarND-Traffic-Signs")))
+
+

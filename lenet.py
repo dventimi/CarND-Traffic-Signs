87587f8e0eb7aa682dbc23b56ad2d4ffd7d817dc
David A. Ventimiglia <dventimi@gmail.com>
2016-12-21 15:24:49 -0800

    Cleanup

--- lenet.py
+++ lenet.py
@@ -1,7 +1,65 @@
-# Load pickled data
-import pickle
+################################################################################
+# Import modules
+################################################################################
 
-# TODO: Fill this in based on where you saved the training and testing data
+from sklearn.utils import shuffle
+from tensorflow.contrib.layers import flatten
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import random
+import tensorflow as tf
+
+################################################################################
+# Define architecture
+################################################################################
+
+EPOCHS = 10
+BATCH_SIZE = 100
+MU = 0
+SIGMA = 0.1
+
+def LeNet(x):    
+    # SOLUTION: Layer 1: Convolutional. Input = 36x36x3. Output = 32x32x6.
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = MU, stddev = SIGMA))
+    conv1_b = tf.Variable(tf.zeros(6))
+    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+    # SOLUTION: Activation.
+    conv1 = tf.nn.relu(conv1)
+    # SOLUTION: Pooling. Input = 32x32x6. Output = 16x16x6.
+    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    # SOLUTION: Layer 2: Convolutional. Output = 43x43x16.
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = MU, stddev = SIGMA))
+    conv2_b = tf.Variable(tf.zeros(16))
+    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+    # SOLUTION: Activation.
+    conv2 = tf.nn.relu(conv2)
+    # SOLUTION: Pooling. Input = 43x43x16. Output = 5x5x16.
+    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
+    fc0   = flatten(conv2)
+    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = MU, stddev = SIGMA))
+    fc1_b = tf.Variable(tf.zeros(120))
+    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
+    # SOLUTION: Activation.
+    fc1    = tf.nn.relu(fc1)
+    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = MU, stddev = SIGMA))
+    fc2_b  = tf.Variable(tf.zeros(84))
+    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
+    # SOLUTION: Activation.
+    fc2    = tf.nn.relu(fc2)
+    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 43), mean = MU, stddev = SIGMA))
+    fc3_b  = tf.Variable(tf.zeros(43))
+    logits = tf.matmul(fc2, fc3_W) + fc3_b
+    return logits
+
+################################################################################
+# Load data
+################################################################################
 
 training_file = '../train.p'
 testing_file = '../test.p'
@@ -13,143 +71,27 @@ with open(testing_file, mode='rb') as f:
     
 X_train, y_train = train['features'], train['labels']
 X_test, y_test = test['features'], test['labels']
-
-### Replace each question mark with the appropriate value.
-
-# TODO: Number of training examples
-
 n_train = X_train.shape[0]
-
-# TODO: Number of testing examples.
-
 n_test = y_train.shape[0]
-
-# TODO: What's the shape of an traffic sign image?
-
 image_shape = X_train.shape[1:]
-
-# TODO: How many unique classes/labels there are in the dataset.
-
-# The labels are numeric (integers), so we could take advantage of the
-# natural ordering of numbers by subtracting the minimum label value
-# from the maximum label value and adding 1 (because both the min
-# value AND the max value are represented among the labels).
-
-n_classes = max(y_train)-min(y_train)+1
-
-# However, by doing this we implicitly assume that every possible
-# label (i.e., integer) in that interval is represented in the training
-# set.  That need not be the case.  Therefore, a more reliable way is
-# to count the number of unique elements within set of labels.
-
-import numpy as np
 n_classes = len(np.unique(y_train))
 
-print("Number of training examples =", n_train)
-print("Number of testing examples =", n_test)
-print("Image data shape =", image_shape)
-print("Number of classes =", n_classes)
-
-
 ################################################################################
-# LENET
+# Pre-process data
 ################################################################################
-
-import math
 
 X_train, y_train           = train['features'][0:math.floor(n_train*0.9),], train['labels'][0:math.floor(n_train*0.9),]
 X_validation, y_validation = train['features'][math.floor(n_train*0.9):,], train['labels'][math.floor(n_train*0.9):,]
 n_train = X_train.shape[0]
 n_validation = X_validation.shape[0]
 
-
 assert(len(X_train) == len(y_train))
 assert(len(X_validation) == len(y_validation))
 assert(len(X_test) == len(y_test))
 
-print()
-print("Image Shape: {}".format(X_train[0].shape))
-print()
-print("Training Set:   {} samples".format(len(X_train)))
-print("Validation Set: {} samples".format(len(X_validation)))
-print("Test Set:       {} samples".format(len(X_test)))
-
-import random
-import numpy as np
-import matplotlib.pyplot as plt
-plt.ion()
-
-index = random.randint(0, len(X_train))
-image = X_train[index].squeeze()
-
-plt.figure(figsize=(1,1))
-plt.imshow(image, cmap="gray")
-print(y_train[index])
-
-from sklearn.utils import shuffle
-
-X_train, y_train = shuffle(X_train, y_train)
-
-import tensorflow as tf
-
-EPOCHS = 10
-BATCH_SIZE = 128
-
-from tensorflow.contrib.layers import flatten
-
-def LeNet(x):    
-    # Hyperparameters
-    mu = 0
-    sigma = 0.1
-    
-    # SOLUTION: Layer 1: Convolutional. Input = 32x32x3. Output = 28x28x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = mu, stddev = sigma))
-    conv1_b = tf.Variable(tf.zeros(6))
-    conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
-
-    # SOLUTION: Activation.
-    conv1 = tf.nn.relu(conv1)
-
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
-    conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Layer 2: Convolutional. Output = 43x43x16.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = mu, stddev = sigma))
-    conv2_b = tf.Variable(tf.zeros(16))
-    conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-    
-    # SOLUTION: Activation.
-    conv2 = tf.nn.relu(conv2)
-
-    # SOLUTION: Pooling. Input = 43x43x16. Output = 5x5x16.
-    conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
-
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
-    fc0   = flatten(conv2)
-    
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = mu, stddev = sigma))
-    fc1_b = tf.Variable(tf.zeros(120))
-    fc1   = tf.matmul(fc0, fc1_W) + fc1_b
-    
-    # SOLUTION: Activation.
-    fc1    = tf.nn.relu(fc1)
-
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = mu, stddev = sigma))
-    fc2_b  = tf.Variable(tf.zeros(84))
-    fc2    = tf.matmul(fc1, fc2_W) + fc2_b
-    
-    # SOLUTION: Activation.
-    fc2    = tf.nn.relu(fc2)
-
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
-    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 43), mean = mu, stddev = sigma))
-    fc3_b  = tf.Variable(tf.zeros(43))
-    logits = tf.matmul(fc2, fc3_W) + fc3_b
-    
-    return logits
-
+################################################################################
+# Set up model
+################################################################################
 
 x = tf.placeholder(tf.float32, (None, 32, 32, 3))
 y = tf.placeholder(tf.int32, (None))
@@ -162,7 +104,6 @@ cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
 training_operation = optimizer.minimize(loss_operation)
-
 
 correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
@@ -177,11 +118,9 @@ def evaluate(X_data, y_data):
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
 
-
 with tf.Session() as sess:
     sess.run(tf.initialize_all_variables())
     num_examples = len(X_train)
-    
     print("Training...")
     print()
     for i in range(EPOCHS):
@@ -190,12 +129,10 @@ with tf.Session() as sess:
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
             sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-            
         validation_accuracy = evaluate(X_validation, y_validation)
         print("EPOCH {} ...".format(i+1))
         print("Validation Accuracy = {:.3f}".format(validation_accuracy))
         print()
-        
     try:
         saver
     except NameError:

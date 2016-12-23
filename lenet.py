84171b6439c119f979972de735493b2d52bd9216
David A. Ventimiglia <dventimi@gmail.com>
2016-12-23 12:04:21 -0800

    Parameterized n_classes

--- lenet.py
+++ lenet.py
@@ -20,37 +20,38 @@ BATCH_SIZE = 100
 MU = 0
 SIGMA = 0.1
 
-def LeNet(x):    
-    # SOLUTION: Layer 1: Convolutional. Input = 36x36x3. Output = 32x32x6.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean = MU, stddev = SIGMA))
+def LeNet(x, n_classes):    
+    # SOLUTION: Layer 1: Convolutional. Input = 32x32xinput_channels. Output = 28x28x6.
+    input_channels = x.get_shape()[3].value
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, input_channels, 6), mean = MU, stddev = SIGMA))
     conv1_b = tf.Variable(tf.zeros(6))
     conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
     # SOLUTION: Activation.
     conv1 = tf.nn.relu(conv1)
 
-    # SOLUTION: Pooling. Input = 32x32x6. Output = 16x16x6.
+    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
     conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-    # SOLUTION: Layer 2: Convolutional. Output = 43x43x16.
+    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
     conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean = MU, stddev = SIGMA))
     conv2_b = tf.Variable(tf.zeros(16))
     conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-
+    
     # SOLUTION: Activation.
     conv2 = tf.nn.relu(conv2)
 
-    # SOLUTION: Pooling. Input = 43x43x16. Output = 5x5x16.
+    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
     conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
     fc0   = flatten(conv2)
-
+    
     # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
     fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean = MU, stddev = SIGMA))
     fc1_b = tf.Variable(tf.zeros(120))
     fc1   = tf.matmul(fc0, fc1_W) + fc1_b
-
+    
     # SOLUTION: Activation.
     fc1    = tf.nn.relu(fc1)
 
@@ -58,13 +59,13 @@ def LeNet(x):
     fc2_W  = tf.Variable(tf.truncated_normal(shape=(120, 84), mean = MU, stddev = SIGMA))
     fc2_b  = tf.Variable(tf.zeros(84))
     fc2    = tf.matmul(fc1, fc2_W) + fc2_b
-
+    
     # SOLUTION: Activation.
     fc2    = tf.nn.relu(fc2)
 
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
-    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, 43), mean = MU, stddev = SIGMA))
-    fc3_b  = tf.Variable(tf.zeros(43))
+    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = n_classes.
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(84, n_classes), mean = MU, stddev = SIGMA))
+    fc3_b  = tf.Variable(tf.zeros(n_classes))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
     return logits
 
@@ -119,7 +120,7 @@ one_hot_y = tf.one_hot(y, n_classes)
 
 rate = 0.001
 
-logits = LeNet(x)
+logits = LeNet(x, n_classes)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits, one_hot_y)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
